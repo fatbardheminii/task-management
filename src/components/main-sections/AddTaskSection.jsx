a67f425434fd6bbc40e37efd5a89cb6987e80af8
fatbardheminii <fatbardh.emini@hotmail.com>
2025-06-02 21:09:23 +0200

    refactor: update AddTaskSection for compatibility with refactored components

--- src/components/main-sections/AddTaskSection.jsx
+++ src/components/main-sections/AddTaskSection.jsx
@@ -2,43 +2,23 @@ import { useState } from "react";
 import { FaPlusCircle, FaMinusCircle } from "react-icons/fa";
 import AddTaskForm from "./AddTaskForm";
 
-const AddTaskSection = ({ onAddTask, onAddTodayTask, onAddThisWeekTask, onAddImportantTask, onAddProject, projects }) => {
+const AddTaskSection = () => {
   const [showForm, setShowForm] = useState(false);
 
   const handleToggle = () => setShowForm(!showForm);
 
   return (
-    <>
-      <section className="add-tasks-sec">
-        <h2>
-          Add Task:{" "}
-          {showForm ? (
-            <FaMinusCircle
-              onClick={handleToggle}
-              className="plus-circle"
-            ></FaMinusCircle>
-          ) : (
-            <FaPlusCircle
-              onClick={handleToggle}
-              className="plus-circle"
-            ></FaPlusCircle>
-          )}
-        </h2>
-        {showForm && (
-          <AddTaskForm
-            onAddTask={(updateFunc) => {
-              onAddTask(updateFunc);
-              setShowForm(false);
-            }}
-            onAddTodayTask={onAddTodayTask}
-            onAddWeekTask={onAddThisWeekTask}
-            onAddImportantTask={onAddImportantTask}
-            onAddProject={onAddProject}
-            projects={projects}
-          />
+    <section className="add-tasks-sec">
+      <h2>
+        Add Task:{" "}
+        {showForm ? (
+          <FaMinusCircle onClick={handleToggle} className="plus-circle" />
+        ) : (
+          <FaPlusCircle onClick={handleToggle} className="plus-circle" />
         )}
-      </section>
-    </>
+      </h2>
+      {showForm && <AddTaskForm setShowForm={setShowForm} />}
+    </section>
   );
 };
 
